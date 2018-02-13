9f5ce2e3d057bb2e57577cf130cbf4d6b559ce24
Daulet Baimbetov <daucloudlab@gmail.com>
2018-02-13 11:50:50 +0600

    version 01.01. Authors.html шаблоны қосылды. Авторлардың тізімін таблица түрінде шығарады

--- src/main/java/kz/kaznitu/lessons/controllers/AuthorController.java
+++ src/main/java/kz/kaznitu/lessons/controllers/AuthorController.java
@@ -4,6 +4,7 @@ import kz.kaznitu.lessons.models.Author;
 import kz.kaznitu.lessons.reposotories.AuthorRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -29,5 +30,12 @@ public class AuthorController {
     @GetMapping("/all")
     public @ResponseBody Iterable<Author> allAuthors(){
         return authorRepository.findAll() ;
+    }
+
+    @GetMapping("/all2")
+    public String allAuthors2(Model model){
+        List<Author> authors = (List<Author>) authorRepository.findAll() ;
+        model.addAttribute("authors", authors) ;
+        return "authors" ;
     }
 }
