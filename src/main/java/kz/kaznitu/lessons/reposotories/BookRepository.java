1005d3e12384a61b41746c2886db03103dc3cb66
kuanysh <kuanyshkntu@gmail.com>
2018-02-20 12:57:18 +0600

    version 3: Add сілтемесі қосылды #4 fixed

--- src/main/java/kz/kaznitu/lessons/reposotories/BookRepository.java
+++
@@ -1,7 +0,0 @@
-package kz.kaznitu.lessons.reposotories;
-
-import kz.kaznitu.lessons.models.Book;
-import org.springframework.data.repository.CrudRepository;
-
-public interface BookRepository extends CrudRepository<Book, Long> {
-}
