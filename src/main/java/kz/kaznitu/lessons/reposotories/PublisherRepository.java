6ef8f272e6587a702dfeb490e83a8dd405391e43
kuanysh <kuanyshkntu@gmail.com>
2018-02-22 10:35:08 +0600

    version 2-4: Book таблицасы құрылды #8 fixed

---
+++ src/main/java/kz/kaznitu/lessons/reposotories/PublisherRepository.java
@@ -0,0 +1,7 @@
+package kz.kaznitu.lessons.reposotories;
+
+import kz.kaznitu.lessons.models.Publisher;
+import org.springframework.data.repository.CrudRepository;
+
+public interface PublisherRepository extends CrudRepository<Publisher,Long> {
+}
