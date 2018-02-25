cda4c83ca2506928393a53071278b8909fa6642f
kuanysh <kuanyshkntu@gmail.com>
2018-02-25 21:39:50 +0600

    version 2-8: Author админ бетіне update қосылды #11 fixed

--- src/main/java/kz/kaznitu/lessons/controllers/AuthorController.java
+++ src/main/java/kz/kaznitu/lessons/controllers/AuthorController.java
@@ -9,12 +9,14 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 @RequestMapping(path = "/demo")
 public class AuthorController {
     @Autowired
     private AuthorRepository authorRepository ;
+    private long a;
 
     @RequestMapping("/add")
     public String showForm(Model model){
@@ -47,12 +49,25 @@ public class AuthorController {
         return "authors" ;
     }
 
+    @PostMapping("/adds")
+    public String addsAuthor(@ModelAttribute Author author){
+        Author author1 = new Author();
+        author1.setId(a);
+        author1.setFirstName(author.getFirstName());
+        author1.setLastName(author.getLastName());
+        author1.setEmail(author.getEmail());
+        authorRepository.save(author1) ;
 
-    /*@RequestMapping(value = "/update",method = RequestMethod.GET)
-    public ModelAndView updateAuthors(@RequestParam("id") long id){
-        authorRepository.findById(id);
-        return new ModelAndView("author_add_form");
-    }*/
+        return "redirect:/demo/all" ;
+    }
+
+    @RequestMapping(value = "/update",method = RequestMethod.GET)
+    public ModelAndView updateAuthors(Model model,@RequestParam("id") long id){
+        a=id;
+        Optional<Author> author = (Optional <Author> ) authorRepository.findById(id);
+        model.addAttribute("authorr",author);
+        return new ModelAndView("update");
+    }
     @RequestMapping(value = "/deleteContact",method = RequestMethod.GET)
     public ModelAndView deleteContact(@RequestParam("id") long idd){
         authorRepository.deleteById(idd);
--- src/main/java/kz/kaznitu/lessons/reposotories/AuthorRepository.java
+++ src/main/java/kz/kaznitu/lessons/reposotories/AuthorRepository.java
@@ -3,5 +3,8 @@ package kz.kaznitu.lessons.reposotories;
 import kz.kaznitu.lessons.models.Author;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface AuthorRepository extends CrudRepository<Author, Long> {
+    Optional<Author> findById(Long id);
 }
